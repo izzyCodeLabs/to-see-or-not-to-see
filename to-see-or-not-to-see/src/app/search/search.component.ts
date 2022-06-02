be8551a6fc16e7d1fd765957ec9c70c9ac32072a
izzyCodeLabs <maepakeiperi@gmail.com>
2022-06-02 14:39:30 -0500

    ADD rudimentary search component
    Can search a movie and display a list of results from imdb-api

--- to-see-or-not-to-see/src/app/search/search.component.ts
+++ to-see-or-not-to-see/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ConnectableObservable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  isLoading = false;
+  movies = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  async fetchMovies(form:NgForm) {
+    console.log("Fetching search results for \"" + form.value.movie + "\"...");
+    const res = await fetch('https://imdb-api.com/en/API/SearchMovie/k_69zqgrrv/' + form.value.movie);
+    const data = await res.json();
+    this.movies = data.results;
+    console.log(data);
   }
 
 }
