9e8c2ec99e6fa52a5ee96ba07ad2cf14f21bb48b
EagleCollin <scoutcollin97@gmail.com>
2022-06-09 11:28:34 -0500

    Routing for To See

--- to-see-or-not-to-see/src/app/app-routing.module.ts
+++ to-see-or-not-to-see/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HaveSeenComponent } from './have-seen/have-seen.component';
 import { SearchComponent } from './search/search.component';
+import { ToSeeComponent } from './to-see/to-see.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/have-seen', pathMatch: 'full' },
   { path: 'have-seen', component: HaveSeenComponent },
   { path: 'search', component: SearchComponent },
+  { path: 'upcoming', component: ToSeeComponent },
 ];
 
 @NgModule({
--- to-see-or-not-to-see/src/app/search/movie-search/movie-search.component.ts
+++ to-see-or-not-to-see/src/app/search/movie-search/movie-search.component.ts
@@ -5,18 +5,17 @@ import { ImdbApiService } from '../../shared/imdb-api.service';
 @Component({
   selector: 'app-movie-search',
   templateUrl: './movie-search.component.html',
-  styleUrls: ['./movie-search.component.css']
+  styleUrls: ['./movie-search.component.css'],
 })
 export class MovieSearchComponent implements OnInit {
   isLoading = false;
   movies;
 
-  constructor(private imdbService:ImdbApiService) { }
+  constructor(private imdbService: ImdbApiService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  async onSearchMovies(form:NgForm) {
+  async onSearchMovies(form: NgForm) {
     this.isLoading = true;
     this.movies = await this.imdbService.fetchSearchedMovies(form);
     this.isLoading = false;
@@ -32,4 +31,5 @@ export class MovieSearchComponent implements OnInit {
     this.isLoading = true;
     this.movies = await this.imdbService.fetchComingSoon();
     this.isLoading = false;
+  }
 }
